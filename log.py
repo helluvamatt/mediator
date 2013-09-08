b48246d7eb5fcd90acb20b07f1f38036c0c49c05
Blake Miller <miller.blake@me.com>
2013-09-08 12:04:27 -0700

    changed behavior of print_queue (now get_queue). instead of printing in function, it returns (and stores) a list

--- log.py
+++ log.py
@@ -1,53 +1,49 @@
 from readjson import read_json
 
-class Log:
+class TorrentQueues:
 
-	def __init__(self, logfile):
+    def __init__(self, settings_file):
 
-		self.logfile = logfile
-		self.json_file = read_json(logfile)
-		self.todo, self.history, self.delta = -1, -1, -1
+        self.settings_file = settings_file
+        self.json_file = read_json(settings_file)
+        self.todo_size, self.history_size, self.delta_size = -1, -1, -1
+        self.todo_list, self.history_list, self.delta_list = [], [], []
+        self.get_queue_size()
 
-	def scan_logs(self):
+    def get_queue_size(self):
 
-		with open(self.json_file["todo-log"]) as todo_items:
-			for self.todo, n in enumerate(todo_items):
-				pass
+        with open(self.json_file["todo-log"]) as todo_items:
+            for self.todo_size, n in enumerate(todo_items):
+                pass
 
-		with open(self.json_file["history-log"]) as history_items:
-			for self.history, n in enumerate(history_items):
-				pass
+        with open(self.json_file["history-log"]) as history_items:
+            for self.history_size, n in enumerate(history_items):
+                pass
 
-		with open(self.json_file["delta-log"]) as delta_items:
-			for self.delta, n in enumerate(delta_items):
-				pass
-		return self.todo + 1, self.history + 1, self.delta + 1
+        with open(self.json_file["delta-log"]) as delta_items:
+            for self.delta_size, n in enumerate(delta_items):
+                pass
+        return self.todo_size + 1, self.history_size + 1, self.delta_size + 1
 
-	def print_logs(self, all=True, todo=False, history=False, delta=False):
-		"""Default behavior is to print all logs;
-		raise flag to print one log explicitly"""
+    def get_queue(self, todo=False, history=False, delta=False):
 
-		self.scan_logs()
+        self.get_queue_size()
 
-		# lower all flag if and only if one specific log flag is raised
-		if todo ^ history ^ delta:
-			all=False
+        # print one
+        if (self.todo_size > -1) and todo:
+            with open(self.json_file["todo-log"]) as todo_items:
+                for torrent in todo_items:
+                    self.todo_list.append(torrent)
+            return self.todo_list
 
-		# print one or all
-		if (self.todo > -1) and (todo or all):
-			print("todo.log(%s):" % (self.todo + 1))
-			with open(self.json_file["todo-log"]) as todo_items:
-				for torrent in todo_items:
-					print torrent.strip('\n')
+        if (self.history_size > -1) and history:
+            with open(self.json_file["history-log"]) as history_items:
+                for torrent in history_items:
+                    self.history_list.append(torrent)
+            return self.history_list
 
-		if (self.history > -1) and (history or all):
-			print("------\nhistory.log(%s):" % (self.history + 1))
-			with open(self.json_file["history-log"]) as history_items:
-				for torrent in history_items:
-					print torrent.strip('\n')
-
-		if (self.delta > -1) and (delta or all):
-			print("-------\ndelta.log(%s):" % (self.delta + 1))
-			with open(self.json_file["delta-log"]) as delta_items:
-				for torrent in delta_items:
-					print torrent.strip('\n')
+        if (self.delta_size > -1) and delta:
+            with open(self.json_file["delta-log"]) as delta_items:
+                for torrent in delta_items:
+                    self.delta_list.append(torrent)
+            return self.delta_list
