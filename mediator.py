4de3e4841198767d4753e02dc6b867c73a2efa9d
Blake Miller <miller.blake@me.com>
2013-09-08 12:01:56 -0700

    now iterating through queue as a list

--- mediator.py
+++ mediator.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python
 
 import os
-from log import Log
+import sh
+from log import TorrentQueues
 
 settings = "settings.json"
 
-def inspect_media_type():
-	pass
+def inspect_media_type(torrent):
+    media_type = raw_input("movie, episode, season, skip, or ignore? [m/e/s/k/i]")
+    print("type is: %s" % media_type)
 
 def main():
-	mylog = Log(settings)
 
-	todo, history, delta = mylog.scan_logs()
-	mylog.print_logs()
+    # first, update the queues and store values
+    tq = TorrentQueues(settings)
 
-if __name__ == "__main__":
-	main()
+    # now, iterate on queue
+    for n, torrent in enumerate(tq.get_queue(todo=True)):
+        print("[%s/%s] %s:" % (n, tq.todo_size, torrent.strip('\n')))
+        inspect_media_type(torrent.strip('\n'))
+
+if __name__ == '__main__':
+    main()
