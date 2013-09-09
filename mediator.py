4059231344559bdaaae83282e3e41e5d53936299
Blake Miller <miller.blake@me.com>
2013-09-08 20:41:07 -0700

    now storing torrent to class instance and verifying which files will be linked

--- mediator.py
+++ mediator.py
@@ -1,24 +1,42 @@
 #!/usr/bin/env python
 
 import os
-import sh
-from log import TorrentQueues
+import logging
+from readjson import read_json
+from queue import TorrentQueues
+from torrent import Torrent, is_media_type, collect_metadata
+from sh import sh
 
 settings = "settings.json"
+json_file = read_json(settings)
 
-def inspect_media_type(torrent):
-    media_type = raw_input("movie, episode, season, skip, or ignore? [m/e/s/k/i]")
-    print("type is: %s" % media_type)
+def verify_source_data(torrent):
+    
+    print("name: %s" % torrent.name)
+    os.chdir(json_file["torrent-library"])
+    for file in os.listdir("%s" % torrent.name):
+        print(file)
 
 def main():
 
     # first, update the queues and store values
     tq = TorrentQueues(settings)
 
-    # now, iterate on queue
-    for n, torrent in enumerate(tq.get_queue(todo=True)):
-        print("[%s/%s] %s:" % (n, tq.todo_size, torrent.strip('\n')))
-        inspect_media_type(torrent.strip('\n'))
+    # now, iterate on each torrent in queue
+    for i, torrent in enumerate(tq.get_queue(todo=True)):
+        
+        torrent = torrent.strip()
+        print("[%s/%s] %s:" % (i, tq.todo_size, torrent))
+        
+        media_type = is_media_type(torrent)
+
+        if media_type:
+            t = Torrent(torrent, media_type)
+
+            t.set_info(**collect_metadata(t))
+
+            verify_source_data(t)
+
 
 if __name__ == '__main__':
     main()
