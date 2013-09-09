333c8c3a4a4545a0029087371aab784cb16e4c36
Blake Miller <miller.blake@me.com>
2013-09-08 20:39:43 -0700

    removed redundant assignment

--- sh.py
+++ sh.py
@@ -8,6 +8,4 @@ def sh(script):
     p = subprocess.Popen(script, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p.communicate()
 
-    ret = [out, err, p.pid, p.returncode]
-    
-    return ret
+    return [out, err, p.pid, p.returncode]
