c46c8670a3f769e734f81e8dc97fd7944f412543
Blake Miller <miller.blake@me.com>
2013-11-14 18:43:47 -0800

    created migrate() function to house merging of log and client torrents. migrate takes one optional argument for specifying alternative client torrent directory location. called from new main(). arg parsing from command line will branch the logic.

--- migrate.py
+++ migrate.py
@@ -2,7 +2,19 @@
 import os
 import re
 
-for torrent in os.listdir("/var/lib/transmission-daemon/info/torrents"):
-    with open("logs/foobar.log" , 'r+a') as log:
-        if not any(re.match('(.*)(?=\..*.torrent)', torrent).group(0) in items for items in log):
-            log.write("%s\n" % re.match('(.*)(?=\..*.torrent)', torrent).group(0))
+def migrate(client_torrents=None):
+
+    if not client_torrents:
+        client_torrents = "/var/lib/transmission-daemon/info/torrents"
+
+    for torrent in os.listdir(client_torrents):
+        with open("logs/foobar.log" , 'r+a') as log:
+            if not any(re.match('(.*)(?=\..*.torrent)', torrent).group(0) in items for items in log):
+                log.write("%s\n" % re.match('(.*)(?=\..*.torrent)', torrent).group(0))
+
+def main():
+
+    migrate()
+    
+if __name__ == '__main__':
+    main()
